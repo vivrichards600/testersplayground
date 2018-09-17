f05a12f29f7ce3dead4bc67e51df416cbbc926ba
Viv Richards <vivrichards600@hotmail.com>
2018-09-17 22:40:58 +0100

    wording changes

--- ChallengeComplete.php
+++ ChallengeComplete.php
@@ -11,11 +11,11 @@ if ( isset( $_SESSION['ChallengeLink'] ) ) {
 <?php include '_includes/header.php';?>
 <main role="main" class="col-md-12 ml-sm-auto col-lg-12 pt-3 px-4">
 	<div class="alert alert-success" role="alert">
-		<h4 class="alert-heading">Challenge Complete!</h4>
+		<h4 class="alert-heading">Challenge complete!</h4>
 		<p>Good job, you successfully completed the challenge.</p>
 		<img src="assets/img/trophy.png" height="160px"/>
 		<hr>
-		<a href='<?php echo $challengeUrl;?>' class='btn btn-success'>Next Challenge 🡪</a>
+		<a href='<?php echo $challengeUrl;?>' class='btn btn-success'>Next challenge 🡪</a>
 	</div>
 
 </main>
