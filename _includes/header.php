e59a53c4c79f3b9f9056b848c8bfee709f56d0a8
Viv Richards <vivrichards600@hotmail.com>
2018-09-18 22:33:21 +0100

    site title updated

--- _includes/header.php
+++ _includes/header.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<title>Chrome DevTools Playground</title>
+	<title>Testers Playground</title>
 
 	<!-- Bootstrap core CSS -->
 	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
