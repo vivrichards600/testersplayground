c4ad41f6c42d27c4e891ac242b07fe42b72cff8f
Viv Richards <vivrichards600@hotmail.com>
2018-09-19 21:39:06 +0100

    link to devtools added

--- end-ed5d427c-fd53-4f11-b8b0-b2b6c1fabe01.php
+++ end-ed5d427c-fd53-4f11-b8b0-b2b6c1fabe01.php
@@ -19,6 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 <div class="alert alert-success" role="alert">
 		<h4 class="alert-heading">That's all folks!</h4>
 		<p>Good job, you successfully completed all the challenges. Please check back soon as I will be adding more!</p>
+    <p>You can find out more about Chrome DevTools by visiting the official page. <a href="https://developers.google.com/web/tools/chrome-devtools/">Chrome DevTools Homepage</a>.
 		<img src="assets/img/trophy.png" height="160px" alt="trophy" />
 		<hr>
 		<p>I'd love your feedback and/or ideas which you can send to <a href="mailto:hello@vivrichards.co.uk">hello@vivrichards.co.uk</p>
